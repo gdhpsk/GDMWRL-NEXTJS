34f0430012c64268c5bc79a29ab49d15545248ca
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2023-06-08 02:32:38 -0700

    push

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -4,7 +4,16 @@ import db from "../../../firebase"
 let count = 0
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    let everything = await db.collection("levels").get()
+    let data = everything.docs.map(async e => {
+      let listData = await db.collection(`levels/${e.id}/list`).get()
+      let list = listData.docs.map(x => x.data())
+      return {
+        ...e.data(),
+        list
+      }
+    })
+    let resolved = await Promise.all(data)
     res.setHeader('Cache-Control', 's-maxage=86400');
-    count++;
-     res.status(200).json(count);
+     res.status(200).json(resolved);
   }
--- src/pages/api/test.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import cache from "../../../cache"
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.json(cache.get("count") ?? 0)
-  }
