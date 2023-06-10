f61f55f42faef2e31df79463aadd3aaf06778c41
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2023-06-10 16:00:21 -0700

    Update firebase-admin.ts

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -2,9 +2,12 @@ import * as application from "firebase-admin/app"
 import * as database from "firebase-admin/firestore"
 
 let apps = application.getApps().length
-
 const firebase = apps ? application.getApp("server") : application.initializeApp({
-    credential: application.applicationDefault(),
+    credential: application.cert({
+        projectId: process.env.project_id,
+        privateKey: process.env.private_key?.replace("\\n", "\n"),
+        clientEmail: process.env.client_email
+    }),
     databaseURL: 'https://mwrl-7b27f-default-rtdb.firebaseio.com',
     databaseAuthVariableOverride: {
       uid: process.env.APIKEY
