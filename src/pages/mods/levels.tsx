5a4b241821ded57b5b00b2a67960e6a1b4f60961
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2023-06-13 22:56:22 -0700

    Update levels.tsx

--- src/pages/mods/levels.tsx
+++ src/pages/mods/levels.tsx
@@ -1,17 +1,17 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth"
 import { useEffect, useState } from "react"
 import ErrorPage from "../404"
-import styles from "../../styles/settings.module.css"
+import styles from "../../styles/levels.module.css"
 import { Button, Form, InputGroup } from "react-bootstrap"
 
 export default function Settings() {
     let [perms, setPerms] = useState<any>(null)
-    let [level, setLevel] = useState<any>(null)
+    let [level, changeLevel] = useState<any>(null);
     let [levels, setLevels] = useState<any>([])
     let auth = getAuth()
     onAuthStateChanged(auth, (u) => {
       if(u) {
-        u?.getIdTokenResult(true).then((e: any) => {
+        u?.getIdTokenResult().then((e: any) => {
           if(e.claims.role == "owner" || e.claims.role == "editor") {
             setPerms(e.claims.role)
           } else {
@@ -42,25 +42,28 @@ export default function Settings() {
     }
 
     useEffect(() => {
+      changeLevel("")
         changeIt();
         (async () => {
+            let levs = []
             let data = await fetch("https://gdmwrl-nextjs.vercel.app/api/levels/75")
             let json = await data.json() 
             let data2 = await fetch("https://gdmwrl-nextjs.vercel.app/api/levels/150")
             let json2 = await data2.json()
-            let data3 = await fetch("https://gdmwrl-nextjs.vercel.app/api/levels/extra")
+            let data3 = await fetch("https://gdmwrl-nextjs.vercel.app/api/levels/extra?page=1")
             let json3 = await data3.json()
-            setLevels([...json, ...json2, ...json3.data])
+            levs.push(...json, ...json2, ...json3.data)
             for(let i = 2; i <= json3.pages; i++) {
                 let ex_data = await fetch(`https://gdmwrl-nextjs.vercel.app/api/levels/extra?page=${i}`)
                 let ex_json = await ex_data.json()
-                setLevels([...levels, ...ex_json.data])
+                levs.push(...ex_json.data)
             }
             let unrated_data = await fetch("https://gdmwrl-nextjs.vercel.app/api/levels/unratedextremes")
             let unrated_json = await unrated_data.json()
-            setLevels([...levels, ...unrated_json].sort((a,b) => a.position - b.position))
+            levs.push(...unrated_json)
+            setLevels(levs.sort((a,b) => a.position - b.position))
         })()
-    })
+    }, [])
 
     if(perms == false) {
         return <ErrorPage></ErrorPage>
@@ -72,14 +75,18 @@ export default function Settings() {
     <hr className="white"/>
     <div id={styles.content}>
       <div id={styles.list}>
-        {levels.map((e: any) => <div key={e.position}>
-            <h1>#{e.position}: {e.name}</h1>
-            <h3>By: {e.host}</h3>
-            <h4>Verifier: {e.verifier}</h4>
+        {levels.map((e: any) => <div key={e.position} onClickCapture={(x) => {
+           changeLevel(e)
+        }}>
+            <h1 className="white">#{e.position}: {e.name}</h1>
+            <h3 className="white">By: {e.host}</h3>
+            <h4 className="white">Verifier: {e.verifier}</h4>
         </div>)}
       </div>
       <div id={styles.show}>
-        
+        {!level ? <h1>No content to display</h1> : <div>
+          <h1 style={{textAlign: "center"}} className="white">{level.name}</h1>
+          </div>}
       </div>
       </div>
     </>
