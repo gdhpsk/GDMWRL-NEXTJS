ec62662b31c17dddb1df319d4b489295a0695016
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2023-06-14 16:04:50 -0700

    Update mongodb.ts

--- mongodb.ts
+++ mongodb.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import levs from "schemas/levels"
 import lead from "schemas/leaderboard"
-mongoose.connection || mongoose.connect(process.env.MONGODB_URI as string);
+mongoose.connect(process.env.MONGODB_URI as string);
 
 export const levels = levs
 export const leaderboard = lead
