536f420d0a5bb95981e34900c75f031fac0c0be2
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2023-06-14 14:22:16 -0700

    Update 75.ts

--- src/pages/api/levels/75.ts
+++ src/pages/api/levels/75.ts
@@ -3,5 +3,6 @@ import {levels} from "../../../../mongodb"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     let everything = await levels.find({position: {"$lte": 75}}).sort({position:1})
+    res.setHeader("Access-Control-Allow-Origin", "*")
      res.status(200).json(everything);
   }
