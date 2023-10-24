9ca75f1e405d41f09faba6c18919e383d397311f
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2023-10-23 17:30:26 -0700

    using X509 auth

--- mongodb.ts
+++ mongodb.ts
@@ -1,7 +1,14 @@
 import mongoose from "mongoose";
 import levs from "schemas/levels"
 import lead from "schemas/leaderboard"
-mongoose.connect(process.env.MONGODB_URI as string);
+
+mongoose.connect(process.env.MONGODB_URI as string, {
+    dbName: "MobileWRList",
+    readPreference: "primaryPreferred",
+    authSource: "$external",
+    authMechanism: "MONGODB-X509",
+    tlsCertificateKeyFile: process.env.keyPath
+} as any);
 
 export const levels = levs
 export const leaderboard = lead
