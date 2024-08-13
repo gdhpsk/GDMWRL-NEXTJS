9ab09730b75e84318fd3396134527bf722457436
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2024-08-12 18:41:38 -0700

    Update main.tsx

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from "react" 
+import {useState, useEffect} from "react"
 import { Container, SSRProvider } from "react-bootstrap"
 import Level from"../components/Level"
 import { NextPageContext } from "next"
